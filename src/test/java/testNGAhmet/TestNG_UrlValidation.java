d6508e1972ca38388af9228fe57b1720cf6fa48f
Hazel Cebelli <hazelcebel@gmail.com>
2022-12-24 00:29:28 -0800

    test2 on 12/24/22

--- src/test/java/testNGAhmet/TestNG_UrlValidation.java
+++ src/test/java/testNGAhmet/TestNG_UrlValidation.java
@@ -1,7 +1,9 @@
 package testNGAhmet;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -21,8 +23,27 @@ public class TestNG_UrlValidation {
 
         driver.get("https://demo.opencart.com/admin/");
         String actualUrl= driver.getCurrentUrl();
-        String expectedUrl="https://demo.opencar.com/admin/";
+        String expectedUrl="https://demo.opencart.com/admin/";
         Assert.assertEquals(actualUrl,expectedUrl);
+
+
+    }
+
+    @Test
+    public void catalogValidation() {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://demo.opencart.com/admin/index.php?route=common/dashboard&user_token=8c959b4b56ad85f3bda8d735122fb8e9");
+        driver.findElement(By.xpath("//a[.=' Catalog']")).click();
+        WebElement pro= driver.findElement(By.xpath("//a[.='Products']"));
+      boolean actualResult= pro.isDisplayed();
+      boolean expectedResult=true;
+
+      Assert.assertEquals(actualResult,expectedResult);
 
 
 
