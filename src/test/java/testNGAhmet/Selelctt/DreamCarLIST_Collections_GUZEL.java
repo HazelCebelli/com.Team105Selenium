85dbd4a44e571ecc3e3f5f65a4340d3ec04e8d27
Hazel Cebelli <hazelcebel@gmail.com>
2022-12-30 15:21:28 -0800

    test2 on 12/30/22 practice

--- src/test/java/testNGAhmet/Selelctt/DreamCarLIST_Collections_GUZEL.java
+++ src/test/java/testNGAhmet/Selelctt/DreamCarLIST_Collections_GUZEL.java
@@ -10,11 +10,13 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class DreamCarLIST {
+public class DreamCarLIST_Collections_GUZEL {
     @Test
-     public void test01(){
+     public void test01() throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
@@ -35,16 +37,35 @@ public class DreamCarLIST {
         Assert.assertEquals(selectPrice.getFirstSelectedOption().getText().trim(),"No max price");
 
        driver.findElement(By.xpath("(//button[@class='sds-button'])[1]")).click();
+        Thread.sleep(2000);
 
-       WebElement sortBy=driver.findElement(By.xpath("//select[@id='sort-dropdown']"));
-       BrowserUtilss.selectBy(sortBy,"list_price", "value");
+       WebElement sortButton=driver.findElement(By.xpath("//select[@id='sort-dropdown']"));
+       BrowserUtilss.selectBy(sortButton,"list_price", "value");
+        Thread.sleep(2000);
 
-       //GUZEL!!! VALIDATE ALL TITLES HAS LEXUS RX 350 has it
-       List<WebElement> alltitles=driver.findElements(By.xpath("//h2[@class='title']"));
-       String actualTitle="";
+
+        //GUZEL!!! VALIDATE ALL TITLES HAS LEXUS RX 350 has it
+       List<WebElement> alltitles=driver.findElements(By.xpath("//h2[@class='title']"));  //20 titles
+        Thread.sleep(2500);
        String expectedTitle="Lexus RX 350";
        for(WebElement title : alltitles){
-           System.out.println(title);
+           System.out.println(title.getText().trim());
+           Assert.assertTrue(title.getText().trim().contains(expectedTitle));
+       }
+
+       //validate the prices are in ascending order
+
+        List<WebElement> allprices=driver.findElements(By.xpath("//span[@class='primary-price']"));
+        Thread.sleep(2500);
+       List<Integer> actualPrice=new ArrayList<>();
+       List<Integer> expectedPrice=new ArrayList<>();
+       for(WebElement priceCar : allprices){
+           actualPrice.add(Integer.parseInt(priceCar.getText().trim().substring(1).replace(",", "")));
+           expectedPrice.add(Integer.parseInt(priceCar.getText().trim().substring(1).replace(",","")));
+
+           Collections.sort(expectedPrice);
+           Assert.assertEquals(actualPrice,expectedPrice);
+
        }
 
 
