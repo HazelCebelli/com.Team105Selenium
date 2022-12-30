6539b3797b597dd549fd18ad145f179b2e54df6f
Hazel Cebelli <hazelcebel@gmail.com>
2022-12-29 23:01:43 -0800

    test2 on 12/29/22 practice

--- src/test/java/testNGAhmet/Selelctt/DreamCar.java
+++ src/test/java/testNGAhmet/Selelctt/DreamCar.java
@@ -1,4 +1,44 @@
 package testNGAhmet.Selelctt;
 
+import BrowserUtils.BrowserUtilss;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class DreamCar {
+    @Test
+     public void test01(){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.cars.com/");
+        WebElement newUsed=driver.findElement(By.id("make-model-search-stocktype"));
+        BrowserUtilss.selectBy(newUsed,"new", "value");
+
+        WebElement make=driver.findElement(By.id("makes"));
+        BrowserUtilss.selectBy(make,"lexus", "value");
+
+        WebElement model=driver.findElement(By.id("models"));
+        BrowserUtilss.selectBy(model,"lexus-rx_350h", "value");
+
+        WebElement price=driver.findElement(By.id("make-model-max-price"));
+       Select selectPrice=new Select(price);
+        Assert.assertEquals(selectPrice.getFirstSelectedOption(),"No max price");
+//
+//        WebElement newUsed=driver.findElement(By.id("make-model-search-stocktype"));
+//        BrowserUtilss.selectBy(newUsed,"new", "value");
+
+
+
+
+
+
+
+    }
 }
