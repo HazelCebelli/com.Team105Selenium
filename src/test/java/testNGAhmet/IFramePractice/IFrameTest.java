c8d884c2ce652652296bc94fa18869280f739a3b
Hazel Cebelli <hazelcebel@gmail.com>
2023-01-04 00:02:09 -0800

    newone on 1/3/23

--- src/test/java/testNGAhmet/IFramePractice/IFrameTest.java
+++ src/test/java/testNGAhmet/IFramePractice/IFrameTest.java
@@ -1,10 +1,12 @@
 package testNGAhmet.IFramePractice;
 
+import BrowserUtils.BrowserUtilss;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class IFrameTest {
@@ -25,13 +27,32 @@ public class IFrameTest {
         WebElement message=driver.findElement(By.id("tinymce"));
         System.out.println(message.getText().trim());
 
-        // get back to main frame
+        // get back to parent frame
         driver.switchTo().parentFrame();
         WebElement header=driver.findElement(By.tagName("h3"));
         System.out.println(header.getText().trim());
+    }
+
+    @Test
+    public void iFrameTest2() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://skpatro.github.io/demo/iframes/");
+        String mainId=driver.getWindowHandle();
+        driver.findElement(By.linkText("Pavilion")).click();
+
+        BrowserUtilss.switchByID(driver,mainId);
+        Actions actions=new Actions(driver);
+        WebElement seleniumButton=driver.findElement(By.xpath("//ul[@id='primary-menu']//span[@class='menu-item-text']/span[.='Selenium' and @class='menu-text']"));
+        actions.moveToElement(seleniumButton).perform();
 
 
 
 
-    }}
+    }
+
+
+}
 
