2d44495fa4c23887df4e768473f4edcedd289693
Hazel Cebelli <hazelcebel@gmail.com>
2023-01-03 21:57:29 -0800

    newone on 1/3/23

--- src/test/java/testNGAhmet/WindowHandle/MultiWindows.java
+++ src/test/java/testNGAhmet/WindowHandle/MultiWindows.java
@@ -1,11 +1,13 @@
 package testNGAhmet.WindowHandle;
 
+import BrowserUtils.BrowserUtilss;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Set;
@@ -15,7 +17,7 @@ public class MultiWindows {
     private Object JavascriptExecutor;
 
     @Test
-    public void multipleWindows() {
+    public void multipleWindowspracticeGUZELLL() {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -37,8 +39,40 @@ public class MultiWindows {
             }
         }
         System.out.println(driver.getTitle());
+    }
+    @Test
+    public void practice2(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
+        driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
+
+        WebElement button4=driver.findElement(By.id("newTabsBtn"));
+        BrowserUtilss.scrollWithJS(driver,button4);
+        button4.click();
+
+        System.out.println(driver.getTitle()); // driver is at 1st page: Window Handles Practice - H Y R Tutorials
+
+        Set<String> allIds=driver.getWindowHandles();
+        for(String id: allIds){
+            driver.switchTo().window(id);
+            if(driver.getTitle().contains("Basic Controls")){
+                break;
+            }
+        }
+        System.out.println(driver.getTitle()); //2nd page: Basic Controls - H Y R Tutorials
+        WebElement header=driver.findElement(By.xpath("//h1[@class='post-title entry-title']"));
+        Assert.assertTrue(header.getText().trim().equals("Basic Controls"));
+
+        for(String id: allIds){
+            driver.switchTo().window(id);
+            if(driver.getTitle().contains("AlertsDemo")){
+                break;
+            }
+        }
 
 
     }
+
 }
